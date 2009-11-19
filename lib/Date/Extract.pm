a8cd0bff55616f2fae2136bbcf7bd283dfee97c4
Shawn M Moore <cpan@sartak.org>
2009-11-19 00:52:34 +0000

    Import of SARTAK/Date-Extract-0.04 from CPAN.
    gitpan-cpan-distribution: Date-Extract
    gitpan-cpan-version:      0.04
    gitpan-cpan-path:         SARTAK/Date-Extract-0.04.tar.gz
    gitpan-cpan-author:       SARTAK
    gitpan-cpan-maturity:     released

--- lib/Date/Extract.pm
+++ lib/Date/Extract.pm
@@ -5,124 +5,14 @@ use DateTime::Format::Natural;
 use List::Util 'reduce';
 use parent 'Class::Data::Inheritable';
 
+our $VERSION = '0.04';
+
 __PACKAGE__->mk_classdata($_) for qw/scalar_downgrade handlers regex/;
 
 sub _croak {
     require Carp;
     Carp::croak @_;
 }
-
-=head1 NAME
-
-Date::Extract - extract probable dates from strings
-
-=head1 VERSION
-
-Version 0.03 released 12 May 08
-
-=cut
-
-our $VERSION = '0.03';
-
-=head1 SYNOPSIS
-
-    my $parser = Date::Extract->new();
-    my $dt = $parser->extract($arbitrary_text)
-        or die "No date found.";
-    return $dt->ymd;
-
-=head1 MOTIVATION
-
-There are already a few modules for getting a date out of a string.
-L<DateTime::Format::Natural> should be your first choice. There's also
-L<Time::ParseDate> which fits some very specific formats. Finally, you can
-coerce L<Date::Manip> to do your bidding.
-
-But I needed something that will take an arbitrary block of text, search it
-for something that looks like a date string, and build a L<DateTime> object
-out of it. This module fills this niche. By design it will produce few false
-positives. This means it will not catch nearly everything that looks like a
-date string. So if you have the string "do homework for class 2019" it won't
-return a L<DateTime> object with the year set to 2019.
-
-=head1 METHODS
-
-=head2 new PARAMHASH => C<Date::Extract>
-
-=head3 arguments
-
-=over 4
-
-=item time_zone
-
-Forces a particular time zone to be set (this actually matters, as "Tuesday"
-on Monday at 11 PM means something different than "Tuesday" on Tuesday at 1
-AM).
-
-By default it will use the "floating" time zone. See the documentation for
-L<DateTime>.
-
-This controls both the input time zone and output time zone.
-
-=item prefers
-
-This argument decides what happens when an ambiguous date appears in the
-input. For example, "Friday" may refer to any number of Fridays. The valid
-options for this argument are:
-
-=over 4
-
-=item nearest
-
-Prefer the nearest date. This is the default.
-
-=item future
-
-Prefer the closest future date.
-
-=item past
-
-Prefer the closest past date. B<NOT YET SUPPORTED>.
-
-=back
-
-=item returns
-
-If the text has multiple possible dates, then this argument determines which
-date will be returned. By default it's 'first'.
-
-=over 4
-
-=item first
-
-Returns the first date found in the string.
-
-=item last
-
-Returns the final date found in the string.
-
-=item earliest
-
-Returns the date found in the string that chronologically precedes any other
-date in the string.
-
-=item latest
-
-Returns the date found in the string that chronologically follows any other
-date in the string.
-
-=item all
-
-Returns all dates found in the string, in the order they were found in the
-strong.
-
-=item all_cron
-
-Returns all dates found in the string, in chronological order.
-
-=back
-
-=cut
 
 sub new {
     my $class = shift;
@@ -153,12 +43,8 @@ sub new {
     return $self;
 }
 
-=for subclasses
-
-This method will combine the arguments of parser->new and extract. Modify the
-"to" hash directly.
-
-=cut
+# This method will combine the arguments of parser->new and extract. Modify the
+# "to" hash directly.
 
 sub _combine_args {
     shift;
@@ -171,32 +57,17 @@ sub _combine_args {
     $to->{time_zone} ||= $from->{time_zone};
 }
 
-=head2 extract, ARGS text => C<DateTime>s
-
-Takes an arbitrary amount of text and extracts one or more dates from it. The
-return value will be zero or more C<DateTime> objects. If called in scalar
-context, only one will be returned, even if the C<returns> argument specifies
-multiple possible return values.
-
-See the documentation of C<new> for the configuration of this method. Any
-arguments passed into this method will trump those from the parser.
-
-You may reuse a parser for multiple calls to C<extract>.
-
-You do not need to have an instantiated C<Date::Extract> object to call this
-method. Just C<< Date::Extract->extract($foo) >> will work.
-
-=cut
-
 sub extract {
     my $self = shift;
     my $text = shift;
     my %args = @_;
 
+    # using extract as a class method
+    $self = $self->new
+        if !ref($self);
+
     # combine the arguments of parser->new and this
-    # don't do this if called as a class method
-    $self->_combine_args($self, \%args)
-        if ref($self);
+    $self->_combine_args($self, \%args);
 
     # when in scalar context, downgrade
     $args{returns} = $self->_downgrade($args{returns})
@@ -334,25 +205,149 @@ sub _extract {
     return @ret;
 }
 
+1;
+
+__END__
+
+=head1 NAME
+
+Date::Extract - extract probable dates from strings
+
+=head1 SYNOPSIS
+
+    my $parser = Date::Extract->new();
+    my $dt = $parser->extract($arbitrary_text)
+        or die "No date found.";
+    return $dt->ymd;
+
+=head1 MOTIVATION
+
+There are already a few modules for getting a date out of a string.
+L<DateTime::Format::Natural> should be your first choice. There's also
+L<Time::ParseDate> which fits many formats. Finally, you can coerce
+L<Date::Manip> to do your bidding.
+
+But I needed something that will take an arbitrary block of text, search it
+for something that looks like a date string, and build a L<DateTime> object
+out of it. This module fills this niche. By design it will produce few false
+positives. This means it will not catch nearly everything that looks like a
+date string. So if you have the string "do homework for class 2019" it won't
+return a L<DateTime> object with the year set to 2019. This is what your users
+would probably expect.
+
+=head1 METHODS
+
+=head2 new PARAMHASH => C<Date::Extract>
+
+=head3 arguments
+
+=over 4
+
+=item time_zone
+
+Forces a particular time zone to be set (this actually matters, as "tomorrow"
+on Monday at 11 PM means something different than "tomorrow" on Tuesday at 1
+AM).
+
+By default it will use the "floating" time zone. See the documentation for
+L<DateTime>.
+
+This controls both the input time zone and output time zone.
+
+=item prefers
+
+This argument decides what happens when an ambiguous date appears in the
+input. For example, "Friday" may refer to any number of Fridays. The valid
+options for this argument are:
+
+=over 4
+
+=item nearest
+
+Prefer the nearest date. This is the default.
+
+=item future
+
+Prefer the closest future date.
+
+=item past
+
+Prefer the closest past date. B<NOT YET SUPPORTED>.
+
+=back
+
+=item returns
+
+If the text has multiple possible dates, then this argument determines which
+date will be returned. By default it's 'first'.
+
+=over 4
+
+=item first
+
+Returns the first date found in the string.
+
+=item last
+
+Returns the final date found in the string.
+
+=item earliest
+
+Returns the date found in the string that chronologically precedes any other
+date in the string.
+
+=item latest
+
+Returns the date found in the string that chronologically follows any other
+date in the string.
+
+=item all
+
+Returns all dates found in the string, in the order they were found in the
+string.
+
+=item all_cron
+
+Returns all dates found in the string, in chronological order.
+
+=back
+
+=back
+
+=head2 extract text, ARGS => C<DateTime>s
+
+Takes an arbitrary amount of text and extracts one or more dates from it. The
+return value will be zero or more C<DateTime> objects. If called in scalar
+context, only one will be returned, even if the C<returns> argument specifies
+multiple possible return values.
+
+See the documentation of C<new> for the configuration of this method. Any
+arguments passed into this method will trump those from the constructor.
+
+You may reuse a parser for multiple calls to C<extract>.
+
+You do not need to have an instantiated C<Date::Extract> object to call this
+method. Just C<< Date::Extract->extract($foo) >> will work.
+
 =head1 FORMATS HANDLED
 
 =over 4
 
-=item today; tomorrow; yesterday
+=item * today; tomorrow; yesterday
 
-=item last Friday; next Monday; previous Sat
+=item * last Friday; next Monday; previous Sat
 
-=item Monday; Mon
+=item * Monday; Mon
 
-=item November 13th, 1986; Nov 13, 1986
+=item * November 13th, 1986; Nov 13, 1986
 
-=item November 13th; Nov 13
+=item * November 13th; Nov 13
 
-=item 13 Nov; 13th November
+=item * 13 Nov; 13th November
 
-=item 1986/11/13; 1986-11-13
+=item * 1986/11/13; 1986-11-13
 
-=item 11-13-86; 11/13/1986
+=item * 11-13-86; 11/13/1986
 
 =back
 
@@ -367,45 +362,7 @@ L<DateTime::Format::Natural>, L<Time::ParseDate>, L<Date::Manip>
 
 =head1 AUTHOR
 
-Shawn M Moore, C<< <sartak at gmail.com> >>
-
-=head1 BUGS
-
-No known bugs at this point.
-
-Please report any bugs or feature requests to
-C<bug-date-extract at rt.cpan.org>, or through the web interface at
-L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=Date-Extract>.
-I will be notified, and then you'll automatically be notified of progress on
-your bug as I make changes.
-
-=head1 SUPPORT
-
-You can find documentation for this module with the perldoc command.
-
-    perldoc Date::Extract
-
-You can also look for information at:
-
-=over 4
-
-=item * AnnoCPAN: Annotated CPAN documentation
-
-L<http://annocpan.org/dist/Date-Extract>
-
-=item * CPAN Ratings
-
-L<http://cpanratings.perl.org/d/Date-Extract>
-
-=item * RT: CPAN's request tracker
-
-L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=Date-Extract>
-
-=item * Search CPAN
-
-L<http://search.cpan.org/dist/Date-Extract>
-
-=back
+Shawn M Moore, C<< <sartak at bestpractical dot com> >>
 
 =head1 ACKNOWLEDGEMENTS
 
@@ -414,12 +371,10 @@ We still use it, but it doesn't quite fill all the particular needs we have.
 
 =head1 COPYRIGHT & LICENSE
 
-Copyright 2007-2008 Best Practical Solutions.
+Copyright 2007-2009 Best Practical Solutions.
 
 This program is free software; you can redistribute it and/or modify it
 under the same terms as Perl itself.
 
 =cut
-
-1; # End of Date::Extract
 
